ba57cdf3377f41c1e4a3b4c3af01ce5046939151
Peter 'Coburn' Fornari <minimalist37@gmail.com>
2016-01-10 04:01:03 -0500

    Robustified SAS7BDAT wrapper

--- nassAPI/sas7bdatWrapper.py
+++ nassAPI/sas7bdatWrapper.py
@@ -3,9 +3,21 @@ import codecs
 from sas7bdat import SAS7BDAT
 
 class SAS7BDATUtil(SAS7BDAT):
+    """
+    SAS7BDAT with extra functionality
+    
+    Currently provides column name decoding and other utility functions
+    """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.column_names_decoded = [codecs.decode(str, "ASCII") for str in self.column_names]
+        
+        #Decode column names
+        self.column_names_decoded = [codecs.decode(db.encoding, db.encoding_errors) for str in self.column_names]
+        #Sort by col_id just to be sure
+        def k(a):
+            return a.col_id
+        self.column_names_decoded.sort(key=k)
 
     def colToIdx(self, colName):
         return self.column_names_decoded.index(colName)
