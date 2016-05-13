75a0bccf9d8811969c21ff9eb2f38e3a3b77ab51
Peter 'Coburn' Fornari <minimalist37@gmail.com>
2016-05-12 22:45:04 -0400

    NassFlaskWeb
    Moved some functionality to own functions
    Fixed presearch alert generation not working properly

--- nassWebApp/nassFlaskWeb.py
+++ nassWebApp/nassFlaskWeb.py
@@ -20,6 +20,105 @@ nassGlobal.updateUserPrefs({
     "rootPath" : os.path.realpath(".."),
 })
 
+def getInitData():
+    """
+    Returns a dict of all the supported values
+    
+    Contains:
+    year: The years we can search over
+    dbName: The name of the databases we can search over
+    colName: A dict of dbNames to colNames we can search for
+    searchValue: [TODO] A dict of colNames to possible values, if no key for colName assume anything supported
+    joinName: The joins that can be used to concatenate logical things
+    compareFunc: The comparison functions that can be used to compare searchValues
+    export: The supported export formats
+    """
+
+    #TODO: Supported shouldn't change while the server is running, can cache this for all requests
+    
+    #SUPPORTED VALUES FOR OPTIONS
+    supported = {}
+    
+    #DB related data
+    supported["year"] = set()
+    supported["dbName"] = set()
+    supported["colName"] = {}
+    #supported["searchValue"] = {"colName" : "searchValue"}
+    for year, yearData in nassGlobal.data["preprocessDBInfo"].items():
+        supported["year"].add(year)
+        for db, dbData in yearData["dbs"].items():
+            supported["dbName"].add(db)
+            supported["colName"][db] = set()
+            for col in dbData["columnNames"]:
+                supported["colName"][db].add(col)
+                #TODO: Add the searchValue stuff
+    
+    #Search related supports
+    supported["joinName"] = [enumObj.name for enumObj in nassSearchTerm.NASSSearchJoin]
+    supported["compareFunc"] = list(nassGlobal.prefs["supportedCompareFuncs"].keys())
+    supported["export"] = ["not", "currently", "implemented"]
+    return supported
+
+def getAlertsFromSearchTerm(searchTerm):
+    """
+    Generates alerts for a search term
+    
+    Alerts are possible ommisions or conflicts that arise with a given set of search
+    terms. An example alert would be that a given year is excluded and the reason that
+    we need to exclude that year from the search such as that a given year does not
+    contain a column that the user is searching for
+    """
+    
+    #Get each of unique dicts inside of the search term
+    searchDicts = searchTerm.allTermDicts()
+    #Get the used values in all dict keys for all dict terms as sets then lists
+    #Currently unneeded
+    """searchLookupDict = {
+        "dbName" : set(),
+        "colName" : set(),
+        "searchValue" : set(),
+        "compareFunc" : set()
+    }
+    for d in searchDicts:
+        for k, v in d.items():
+            if not k in searchLookupDict:
+                searchLookupDict[k] = set()
+            searchLookupDict[k].add(v)
+    for k, v in searchLookupDict.items():
+        searchLookupDict[k] = list(v)"""
+    
+    
+    #Do the presearch, generate alerts
+    alerts = []
+    #Alerts for years excluded
+    for year, yearData in nassGlobal.data["preprocessDBInfo"].items():
+        for searchDict in searchDicts:
+            #ALERT TYPE: Database not in year of range
+            if not searchDict["dbName"] in yearData["dbs"].keys():
+                alerts.append({
+                    "name" : "Year " + year + " excluded",
+                    "shortName" : year + " EXCL",
+                    "type" : "exclusion",
+                    "description" : "The year " + year + " does not contain an entry for database " + db + " (which is in your search terms) and so year " + year + " will be excluded."
+                })
+                break
+            
+            #ALERT TYPE: Column not in database of a given year
+            if not searchDict["colName"] in yearData["dbs"][searchDict["dbName"]]["columnNames"]:
+                alerts.append({
+                    "name" : "Year " + year + " excluded",
+                    "shortName" : year + " EXCL",
+                    "type" : "exclusion",
+                    "description" : "The year " + year + " contains database " + db + " but does not have column " + col + " and so will be excluded."
+                })
+                break
+                
+    return alerts
+
+
+
+#===
+#FLASK ROUTES
 def jsonToNASSSearch(jsonData):
     jsonObj = json.loads(jsonData)
     translateObj = {
@@ -57,28 +156,8 @@ def init():
     """
     Ajax response for init request. Responds with all supported operations and data
     """
-
-    #SUPPORTED VALUES FOR OPTIONS
-    supported = {}
     
-    #DB related data
-    supported["year"] = set()
-    supported["dbName"] = set()
-    supported["colName"] = {}
-    #supported["searchValue"] = {"colName" : "searchValue"}
-    for year, yearData in nassGlobal.data["preprocessDBInfo"].items():
-        supported["year"].add(year)
-        for db, dbData in yearData["dbs"].items():
-            supported["dbName"].add(db)
-            supported["colName"][db] = set()
-            for col in dbData["columnNames"]:
-                supported["colName"][db].add(col)
-                #TODO: Add the searchValue stuff
-    
-    #Search related supports
-    supported["joinName"] = [enumObj.name for enumObj in nassSearchTerm.NASSSearchJoin]
-    supported["compareFunc"] = list(nassGlobal.prefs["supportedCompareFuncs"].keys())
-    supported["export"] = ["not", "currently", "implemented"]
+    supported = getInitData()
     
     class SetEncoder(json.JSONEncoder):
         def default(self, obj):
@@ -93,55 +172,15 @@ def presearch():
     """
     Ajax response for a presearch request
     
-    A presearch takes a search and returns and alerts that may occur given the
-    data it is searching. An example alert would be that a given year is excluded
-    and the reason that we need to exclude that year from the search
+    A presearch takes a search and returns any information to give the user some
+    sort of prediction into search results or problems. Currently returns all the
+    alerts (conflicts and ommisions) that their search will causes. See getAlertsFromSearchTerm
+    for more information
     """
 
     requestData = codecs.decode(request.data, "utf_8");
     searchTerm = jsonToNASSSearch(requestData)
-    searchDicts = searchTerm.allTermDicts()
-    searchLookupDict = {}
-    for d in searchDicts:
-        for k, v in d.items():
-            if not k in searchLookupDict:
-                searchLookupDict[k] = set()
-            searchLookupDict[k].add(v)
-    for k, v in searchLookupDict.items():
-        searchLookupDict[k] = list(v)
-    
-    
-    #Do the presearch, generate alerts
-    alerts = []
-    #Alerts for years excluded
-    for year, yearData in nassGlobal.data["preprocessDBInfo"].items():
-        shouldBreak = False
-        #ALERT TYPE: Database not in year of range
-        for db in searchLookupDict["dbName"]:
-            if not db in yearData["dbs"].keys():
-                alerts.append({
-                    "name" : "Year " + year + " excluded",
-                    "shortName" : year + " EXCL",
-                    "type" : "exclusion",
-                    "description" : "The year " + year + " does not contain an entry for database " + db + " (which is in your search terms) and so year " + year + " will be excluded."
-                })
-                break
-                
-            #ALERT TYPE: Column not in database of a given year
-            for col in searchLookupDict["colName"]:
-                if not col in yearData["dbs"][db]["columnNames"]:
-                    alerts.append({
-                        "name" : "Year " + year + " excluded",
-                        "shortName" : year + " EXCL",
-                        "type" : "exclusion",
-                        "description" : "The year " + year + " contains database " + db + " but does not have column " + col + " and so will be excluded."
-                    })
-                    shouldBreak = True
-                    break
-            if shouldBreak:
-                shouldBreak = False
-                break
-                
+    alerts = getAlertsFromSearchTerm(searchTerm)
     
     return json.dumps(alerts)
  
