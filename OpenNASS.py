6a052122fec84abf945d30936a2968d29574b09d
Peter 'Coburn' Fornari <minimalist37@gmail.com>
2016-05-31 16:18:21 -0400

    Added OpenNASS.py and updated Readme to reflect this

--- OpenNASS.py
+++ OpenNASS.py
@@ -1,15 +1,34 @@
 
 
 #All the functionality for OpenNASS, the actual system using the API and Flask web stuff
-def setup():
-    #Download the stuff from NASS website
-
-    #Preprocess all the information
+def preprocess():
+    """
+    Preprocesses all the databases stored in the NASS DB folder
+    """
     from nassAPI import nassPreprocess
     nassPreprocess.main()
 
+def setup():
+    print("This action is not fully implemented yet")
+    return
+    """
+    Sets up this copy of OpenNASS
+    
+    Downloads all the files from the FTP and preprocesses them
+    """
+    #Download the stuff from NASS website
+    #Preprocess the databases
+    preprocess()
 
 def run():
+    print("This action is not fully implemented yet")
+    return
+    """
+    Opens the NASS GUI
+    
+    Runs the NASS Flask web server and opens the users browser to the page
+    """
+    
     #Open OpenNASS and runs it
     import os
     os.chdir("./nassWebApp")
@@ -19,10 +38,27 @@ def run():
     
     import webbrowser
     webbrowser.open("http://127.0.0.1:5000")
+
+def printUsage():
+    print("Usage: python OpenNASS.py [action=run,setup]")
     
 def main():
-    run()
+    #Check arguments length
+    if len(sys.argv) != 2:
+        print("Not enough arguments")
+        printUsage()
+        return
+    
+    #Check action
+    action = sys.argv[1]
+    if action in ["main", "printUsage"] or not action in globals() or not hasattr(globals()[action], '__call__'):
+        print("Action does not exist")
+        printUsage()
+        return
+        
+    #Call the action
+    globals()[action].__call__()
     
     
 if __name__ == "__main__":
-    print("This file is not fully implemented yet")
+    main()
--- README.md
+++ README.md
@@ -7,19 +7,16 @@ A tool to search NHTSA's NASS database as well as export the data
 1. Open a command window and run `pip install -r requirements.txt`
  1. To use pip, Python must be installed and the Python directory should be on the PATH variable.
 2. Download the NASS database folders (the folders with the names of the years) from the FTP and put them in a folder
+ 1. The FTP site can be found here ftp://ftp.nhtsa.dot.gov/NASS/
 3. Configure user preferences (specifically the folder for the databases downloaded)
  1. User preferences are configured in each script individually through calls to nassGlobal.py's `updateUserPrefs`
-4. Run `python nassPreprocess.py`
+4. Run `python OpenNASS.py preprocess`
  1. This will create the preprocessed index of all the databases
  2. Hit y to extract all self-extracting archives if you trust the source of your database files.
 
 ###RUN THE GUI
 
-1. Open a command window and navigate to `./nassWebApp`
-2. Run `python nassFlaskWeb.py`
-3. The Flask web server should now be running.
-3. Open a browser and navigate to `127.0.0.1:5000`
-4. Use the GUI to perform searches.
+1. Run `python OpenNASS.py run`
 
 ###CREATE A PROGRAM:
 
