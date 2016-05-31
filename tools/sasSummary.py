f034f6b86d3f30ab19fc029089fce6df331b2a75
Peter 'Coburn' Fornari <minimalist37@gmail.com>
2016-05-31 13:34:58 -0400

    Added a few options to sasSummary for testing purposes

--- tools/sasSummary.py
+++ tools/sasSummary.py
@@ -1,6 +1,7 @@
 #A simple script to explore SAS7BDAT files interactively
 #Windows only due to msvcrt
 #Doesn't handle end of file (IteratorStop exception)
+import os.path
 import sys
 import msvcrt
 
@@ -8,46 +9,74 @@ from sas7bdat import SAS7BDAT
 
 def aStr(a):
     return "[" + ",".join(a) + "]"
-
-if __name__ == "__main__":
+    
+def main():
     print("Sas7bDat Summary == WINDOWS ONLY")
-    print("f for forward, b for backward, q to quit\n")
-    if len(sys.argv) != 2:
-        print("Usage: sasSummary.py [.sas7bdat file]")
+    if len(sys.argv) < 3:
+        print("Usage: sasSummary.py [mode] [.sas7bdat file or filename]")
         sys.exit()
         
-    filePath = sys.argv[1]
-    with SAS7BDAT(filePath, skip_header=True) as db:
-        print("COLUMNS")
-        #Decode some of the information
-        cols = [col.name.decode(db.encoding, db.encoding_errors) for col in db.columns]
-        print(aStr(cols))
-        print("")
-        
-        dbPos = 0
-        lookPos = 0
-        rowCache = dict()
-        inChar = None
-        dbItr = iter(db)
-        while True:
-            #Get current look row
-            if lookPos >= dbPos:
-                row = [str(a) for a in dbItr.__next__()]
-                dbPos+=1
-                rowCache[lookPos] = row
-            else:
-                row = rowCache[lookPos]
+    filePath = sys.argv[2]
+    if(sys.argv[1] == "i" or sys.argv[1] == "e"):
+        with SAS7BDAT(filePath, skip_header=True) as db:
+            if(sys.argv[1] == "i"):
+                print("f for forward, b for backward, q to quit\n")
+                print("COLUMNS")
+                #Decode some of the information
+                cols = [col.name.decode(db.encoding, db.encoding_errors) for col in db.columns]
+                print(aStr(cols))
+                print("")
                 
-            print("P:" + str(lookPos) + " " + aStr(row))
+                dbPos = 0
+                lookPos = 0
+                rowCache = dict()
+                inChar = None
+                dbItr = iter(db)
+                while True:
+                    #Get current look row
+                    if lookPos >= dbPos:
+                        row = [str(a) for a in dbItr.__next__()]
+                        dbPos+=1
+                        rowCache[lookPos] = row
+                    else:
+                        row = rowCache[lookPos]
+                        
+                    print("P:" + str(lookPos) + " " + aStr(row))
+                    
+                    #Get the control character and do stuff
+                    inChar = msvcrt.getch().decode("ascii", "strict")
+                    
+                    if inChar == "q":
+                        sys.exit()
+                    elif inChar == "b":
+                        lookPos-=1
+                        if lookPos < 0:
+                            lookPos = 0
+                    elif inChar == "f":
+                        lookPos+=1
+            elif(sys.argv[1] == "e"):
+                exportFilePath = os.path.splitext(filePath)[0] + "_export.csv"
+                print("EXPORTING TO " + exportFilePath)
+                f = open(exportFilePath, "w")
             
-            #Get the control character and do stuff
-            inChar = msvcrt.getch().decode("ascii", "strict")
+    if(sys.argv[1] == "ea"): #Must be performed in the base directory
+        filePathBase = "./nassDB/"
+        filePathSecondBase = "/Unformatted Data/"
+        exportFileName = sys.argv[2]
+        
+        writeFilePath = "./" + exportFileName + ".csv_but_with_tildes"
+        f = open(writeFilePath, "w")
+        f.write("\n")
+        print("EXPORTING TO " + writeFilePath)
+        for year in ["2010", "2011", "2012", "2013"]:
+            readFilePath = filePathBase + year + filePathSecondBase + exportFileName + ".sas7bdat"
+            print("EXPORTING FROM " + readFilePath)
+            with SAS7BDAT(readFilePath, skip_header=True) as db:
+                for row in db:
+                    row = [str(a) for a in row]
+                    f.write("~".join(row) + "~" + year + "\n")
             
-            if inChar == "q":
-                sys.exit()
-            elif inChar == "b":
-                lookPos-=1
-                if lookPos < 0:
-                    lookPos = 0
-            elif inChar == "f":
-                lookPos+=1
+
+#Argv[1] == i (interactive), e (export), ea (export all)
+if __name__ == "__main__":
+    main()
